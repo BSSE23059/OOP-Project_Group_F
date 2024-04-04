9c14c204efca2f15f9cef29f0b287ee90c8cb579
TOSHIBA <bsse23059@itu.edu.pk>
2024-04-04 23:12:14 +0500

    BSSE23059

--- Block.cpp
+++ Block.cpp
@@ -1,5 +1,2 @@
-//
-// Created by TOSHIBA on 4/4/2024.
-//
 
 #include "Block.h"
--- UnionCouncil.h
+++ UnionCouncil.h
@@ -1,10 +1,14 @@
 #ifndef OOP_PROJECT_GROUP_F_UNIONCOUNCIL_H
 #define OOP_PROJECT_GROUP_F_UNIONCOUNCIL_H
 #include <iostream>
+#include <vector>
+#include "Block.h"
 using namespace std;
 
 class UnionCouncil {
 private:
+    string unionCouncilCode;
+    vector<Block> unionCouncilBlocks;
 
 };
 
