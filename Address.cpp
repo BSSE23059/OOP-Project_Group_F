0308d21b066ad031f1f5f92cd10ac461ee60ed1e
bsse23057 <143398173+bsse23057@users.noreply.github.com>
2024-04-04 09:36:37 +0500

    Murtaza //
    getter and setter of adress.h and cpp

--- Address.cpp
+++ Address.cpp
@@ -1,5 +1,32 @@
-//
-// Created by TOSHIBA on 4/3/2024.
-//
-
 #include "Address.h"
+Address::Address() {
+    province="";
+    district="";
+    unionCouncil="";
+    block="";
+    houseNum=0;
+    streetNum=0;
+}
+Address::Address(std::string province, std::string district, std::string uncouncil, std::string block, int housenm,int streetnm)
+{
+    setprovice(province);
+    set_disrict(district);
+    set_Unioncouncil(uncouncil);
+    set_block(block);
+    set_HouseNum(housenm);
+    set_StreetNum(streetnm);
+}
+
+void Address::setprovice(std::string prvn) {province=prvn;}
+void Address::set_Unioncouncil(std::string uncoun) {unionCouncil=uncoun;}
+void Address::set_block(std::string blck) {block=blck;}
+void Address::set_StreetNum(int stnum) {streetNum=stnum;}
+void Address::set_HouseNum(int hnum) {houseNum=hnum;}
+void Address::set_disrict(std::string dist) {district=dist;}
+
+int Address::get_HouseNum() {return houseNum;}
+int Address::get_StreetNum() {return streetNum;}
+string Address::get_Unioncouncil() {return unionCouncil;}
+string Address::get_block() {return block;}
+string Address::get_disrict() {return district;}
+string Address::get_provice() {return province;}
